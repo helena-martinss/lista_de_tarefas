5b53a35b2684a6c43c95a6ea871caf6ba421b1d3
helena <helena257424@gmail.com>
2024-11-22 12:00:02 -0300

    Inúmeras alterações visuais

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
 
+    caixaDesmarcada = 'caixa_desmarcada.jpg'
+    caixaMarcada = 'caixa_marcada.jpg'
+
     $('form').on('submit', function(e) {
         e.preventDefault();
 
         const nomeTarefa = $('#nome-tarefa').val();
         const novaTarefa = $('<li></li>');
 
-        $(`<p>${nomeTarefa}</p>`).appendTo(novaTarefa);
+        $(`<img class="imagem-caixa" src="caixa_desmarcada.jpg" alt="Caixa desmarcada"><span class="word">${nomeTarefa}</span>`).appendTo(novaTarefa);
         $(novaTarefa).appendTo('ul');
         $('#nome-tarefa').val('');
-        
-        $('ul li').click(function() {
-            $(this).toggleClass('line-through');
-        })    
-    })
-})
+    });
+
+    $('ul').on('click', '.imagem-caixa', function() {
+        var currentSrc = $(this).attr('src');
+        if (currentSrc == caixaDesmarcada) {
+            $(this).attr('src', caixaMarcada);
+            $(this).siblings('.word').addClass('line-through');
+        } else {
+            $(this).attr('src', caixaDesmarcada);
+            $(this).siblings('.word').removeClass('line-through');
+        }
+
+    });
+});
